d699da3b45c151d52f429ffbde1d625fcaed096d
alex-goldschmidt <83550948+alex-goldschmidt@users.noreply.github.com>
2023-04-02 23:54:02 -0400

    Add menu-icon toggle for sidebar in smaller screens

--- client/src/components/sidebar.js
+++ client/src/components/sidebar.js
@@ -1,22 +1,54 @@
 import "../styles/sidebar.css";
 import React from "react";
+import CloseIcon from "@mui/icons-material/Close";
+import MenuIcon from "@mui/icons-material/Menu";
+import Button from "@mui/material/Button";
+import { useState, useEffect } from "react";
 
 const Sidebar = ({ Memes }) => {
+  const [showSidebar, setShowSidebar] = useState(true);
   let firstMemeTemplate = "";
   if (Memes.length > 0) {
     // Get the first meme and its template
     const firstMeme = Memes[0];
     firstMemeTemplate = firstMeme.template;
   }
+
+  const handleCloseSidebar = () => {
+    setShowSidebar(false);
+  };
+
+  const handleOpenSidebar = () => {
+    setShowSidebar(true);
+  };
+
+  useEffect(() => {
+    const screenWidth = window.innerWidth;
+    if (screenWidth <= 540) {
+      setShowSidebar(false);
+    }
+  }, []);
+
   return (
-    <div className="sidebar">
-      <h2 className="StartNewChat">+ Start New Chat</h2>
-      {firstMemeTemplate && (
-        <React.Fragment>
-          <p className="SavedChat">{firstMemeTemplate}</p>
-        </React.Fragment>
+    <>
+      {showSidebar ? (
+        <div className="sidebar">
+          <div className="sidebarHeader">
+            <Button variant="outlined" className="StartNewChat">
+              New Chat
+            </Button>
+            <CloseIcon className="close" onClick={handleCloseSidebar} />
+          </div>
+          {firstMemeTemplate && (
+            <>
+              <p className="SavedChat">{firstMemeTemplate}</p>
+            </>
+          )}
+        </div>
+      ) : (
+        <MenuIcon className="menu-icon" onClick={handleOpenSidebar} />
       )}
-    </div>
+    </>
   );
 };
 
